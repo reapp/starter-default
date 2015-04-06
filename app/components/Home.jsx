47f614c57c9b8224ce271b59489f36fdb35148ef
natew <natewienert@gmail.com>
2015-04-06 15:01:03 -0700

    router simpler

--- app/components/Home.jsx
+++ app/components/Home.jsx
@@ -7,7 +7,7 @@ class Home extends React.Component {
         <View title="REAPP_NAME">
           <p>Hello, World!</p>
 
-          <Button onTap={() => this.context.router.transitionTo('sub')}>
+          <Button onTap={() => this.router().transitionTo('sub')}>
             Go to sub view
           </Button>
         </View>
